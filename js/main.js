dc466791e0d6da83cccba2c1c944e8cacaa56bb5
Capi Etheriel <barraponto@gmail.com>
2016-03-10 20:02:53 -0300

    improving the template

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
     eventTemplate = Handlebars.compile($('#event-template').html());
+
     $.ajax('events-ny.json').done(function(data){
         $eventList = $('.event-list');
         data.forEach(function(event){
+            console.log(date);
+            if (event.price === 0) { event.price = 'FREE'; }
+            if (event.venue) {
+                event.whereabouts = event.venue;
+            } else {
+                event.whereabouts = event.address;
+            }
             $eventList.append(eventTemplate(event));
         });
     });
